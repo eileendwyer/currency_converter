96534396ce0e8fbadb61021b9d266630ed3385ae
Eileen Dwyer <eolson@smalltalku.com>
2016-06-01 17:10:41 -0400

    final revisions

--- currency_converter1.py
+++ currency_converter1.py
@@ -5,38 +5,51 @@ class Money:
             self.currency = currency
 
         def __add__(self, other):
+            return self.converted_value() + other.converted_value()
 
-            def __str__(self):
-                return ("{} {}'s".format(self.value, self.currency)
+        def __sub__(self, other):
+            return self.converted_value() - other.converted_value()
+
+        def __mul__(self, other):
+            return self.converted_value() * other.converted_value()
+
+        def __gt__(self, other):
+            return self.converted_value() * other.converted_value()
+
+        def __ge__(self, other):
+            return self.converted_value() * other.converted_value()
+
+        def __le__(self, other):
+            return self.converted_value() * other.converted_value()
+
+        def __lt__(self, other):
+            return self.converted_value() * other.converted_value()
+
+        def __str__(self):
+            return ("{} {}'s".format(self.value, self.currency)
                       +  " equals {} USD.".format(self.converted_value))
 
-        def __mul__(self, value):
+        def converted_value(self):
+            if self.currency == "XTC":
+                return self.value * 530.696
+            elif self.currency == "EUR":
+                return self.value * 1.11315
+            elif self.currency == "JPY":
+                return self.value * .00905
+            else:
+                return self.value * 1
 
-            def converted_value(self, value):
-                if self.currency == "XTC":
-                    return self.converted_value * 530.696
-                elif self.currency == "EUR":
-                    return self.converted_value * 1.11315
-                elif self.currency == "JPY":
-                    return self.converted_value * .00905
-                else:
-                    return self.value * 1
-
-instance_1 = Money(25, "XTC")
-instance_1.currency = "JPY"
+instance_1 = Money(25, "USD")
+#print(instance_1.value)
 #print(instance_1.currency)
-instance_2 = Money(8, "JPY")
-print(instance_2.value)
-instance_3 = instance_1.currency + instance_2.currency
-if instance_1.currency >= instance_2.currency:
-    print(instance_1 - instance_2)
-elif instance_1.currency <= instance_2.currency:
-    print(instance_1.currency * instance_2.currency)
-elif instance_1.currency == instance_2.currency:
-    print(instance_1.currency**2)
-elif instance_1.currency != instance_2.currency:
-    print("They are not equal.")
-
-
+print(instance_1.converted_value())
+instance_2 = Money(8, "EUR")
+#print(instance_2.value)
+instance_3 = instance_1 + instance_2
+print(instance_3)
+print(instance_1 - instance_2)
+print(instance_1 * instance_2)
+print(instance_1 >= instance_2)
+print(instance_1 <= instance_2)
 
 
